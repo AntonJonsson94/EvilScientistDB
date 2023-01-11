7bcabfbb7e787c59752338f733f96b463a84a23e
Anton Jonsson <94antjon@skola.boras.se>
2023-01-11 09:37:05 +0100

    created new dev branch with new features

--- dist/evil.js
+++ dist/evil.js
@@ -1,3 +1,23 @@
 "use strict";
-const Arne = { name: "Arne", age: 54, henchmen: 544, info: "Mad scientist with anger issues" };
-console.log(Arne);
+const sounds = [];
+const Scientists = [
+    { name: "Hjalmar", age: 50, henchmen: 5000, info: "Stuff" },
+    { name: "Balder", age: 80, henchmen: 50000, info: "Stuffs" },
+];
+console.log(Scientists);
+const form = document.querySelector("#form");
+const newScientistName = document.querySelector("#name");
+const newScientistAge = document.querySelector("#age");
+const newScientistHenchmen = document.querySelector("#henchmen");
+const newScientistInfo = document.querySelector("#info");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newScientist = {
+        name: newScientistName.value,
+        age: parseInt(newScientistAge.value),
+        henchmen: parseInt(newScientistHenchmen.value),
+        info: newScientistInfo.value,
+    };
+    Scientists.push(newScientist);
+    console.log(Scientists);
+});
--- src/evil.ts
+++ src/evil.ts
@@ -1,11 +1,35 @@
 type evilScientist = {
-    name: string,
-    age: number,
-    henchmen: number,
-    info: string,
-} 
+    name: string;
+    age: number;
+    henchmen: number;
+    info: string;
+};
 
-const Arne: evilScientist = {name: "Arne", age: 54, henchmen: 544, info: "Mad scientist with anger issues"}
+const sounds: string[] = [];
 
+const Scientists: evilScientist[] = [
+    { name: "Hjalmar", age: 50, henchmen: 5000, info: "Stuff" },
+    { name: "Balder", age: 80, henchmen: 50000, info: "Stuffs" },
+];
 
-console.log(Arne)
+console.log(Scientists);
+
+const form = document.querySelector("#form") as HTMLFormElement;
+const newScientistName = document.querySelector("#name") as HTMLInputElement;
+const newScientistAge = document.querySelector("#age") as HTMLInputElement;
+const newScientistHenchmen = document.querySelector(
+    "#henchmen"
+) as HTMLInputElement;
+const newScientistInfo = document.querySelector("#info") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+    const newScientist: evilScientist = {
+        name: newScientistName.value,
+        age: parseInt(newScientistAge.value),
+        henchmen: parseInt(newScientistHenchmen.value),
+        info: newScientistInfo.value,
+    };
+    Scientists.push(newScientist);
+    console.log(Scientists);
+});
