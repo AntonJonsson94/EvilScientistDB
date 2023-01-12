8a5f7e3472f6c716e01811c4594b5af5410fdfe1
Anton Jonsson <94antjon@skola.boras.se>
2023-01-12 14:18:29 +0100

    Added ts functionality

--- dist/evil.js
+++ dist/evil.js
@@ -3,15 +3,66 @@ const sounds = [];
 const Scientists = [
     { name: "Hjalmar", age: 50, henchmen: 5000, info: "Stuff" },
     { name: "Balder", age: 80, henchmen: 50000, info: "Stuffs" },
+    { name: "Torbjörn", age: 45, henchmen: 6540, info: "Likes beef" },
+    {
+        name: "Alfons",
+        age: 65,
+        henchmen: 100100100,
+        info: "Born and raised in Dalarna",
+    },
 ];
+function printScientists() {
+    let cardContainer = document.querySelector("#scientist-cards");
+    cardContainer.innerHTML = "";
+    for (let i = 0; i < Scientists.length; i++) {
+        const newScientistCard = document.createElement("div");
+        newScientistCard.setAttribute("id", "column");
+        const cardlist = document.createElement("ul");
+        const nameList = document.createElement("div");
+        const cardButton = document.createElement("button");
+        cardButton.innerHTML = "More details";
+        nameList.innerHTML = Scientists[i].name;
+        cardContainer.appendChild(newScientistCard);
+        newScientistCard.appendChild(cardlist);
+        cardlist.appendChild(nameList);
+        nameList.appendChild(cardButton);
+        cardButton.addEventListener("click", () => {
+            printSelectedScientist(i);
+        });
+    }
+}
+printScientists();
+function printSelectedScientist(index) {
+    console.log("Clicked on index:" + index);
+    const selectedScientist = Scientists[index];
+    let detailedInfoContainer = document.querySelector("#detailed-info");
+    detailedInfoContainer.innerHTML = "";
+    const selectedScientistCard = document.createElement("div");
+    selectedScientistCard.setAttribute("id", "selected-scientist-card");
+    const selectedScientistName = document.createElement("h4");
+    selectedScientistName.innerHTML = selectedScientist.name;
+    const selectedScientistCardList = document.createElement("ul");
+    const selectedScientistAge = document.createElement("div");
+    selectedScientistAge.innerHTML = selectedScientist.age.toString();
+    const selectedScientistHenchmen = document.createElement("li");
+    selectedScientistHenchmen.innerHTML = selectedScientist.henchmen.toString();
+    const selectedScientistInfo = document.createElement("li");
+    selectedScientistInfo.innerHTML = selectedScientist.info;
+    detailedInfoContainer.appendChild(selectedScientistCard);
+    selectedScientistCard.appendChild(selectedScientistName);
+    selectedScientistName.appendChild(selectedScientistCardList);
+    selectedScientistCardList.appendChild(selectedScientistAge);
+    selectedScientistCardList.appendChild(selectedScientistHenchmen);
+    selectedScientistCardList.appendChild(selectedScientistInfo);
+}
 console.log(Scientists);
-const form = document.querySelector("#form");
+const submitButton = document.querySelector("#addbutton");
 const newScientistName = document.querySelector("#name");
 const newScientistAge = document.querySelector("#age");
 const newScientistHenchmen = document.querySelector("#henchmen");
 const newScientistInfo = document.querySelector("#info");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
     const newScientist = {
         name: newScientistName.value,
         age: parseInt(newScientistAge.value),
@@ -20,4 +71,5 @@ form.addEventListener("submit", (e) => {
     };
     Scientists.push(newScientist);
     console.log(Scientists);
+    printScientists();
 });
--- src/evil.ts
+++ src/evil.ts
@@ -10,11 +10,102 @@ const sounds: string[] = [];
 const Scientists: evilScientist[] = [
     { name: "Hjalmar", age: 50, henchmen: 5000, info: "Stuff" },
     { name: "Balder", age: 80, henchmen: 50000, info: "Stuffs" },
+    { name: "Torbjörn", age: 45, henchmen: 6540, info: "Likes beef" },
+    {
+        name: "Alfons",
+        age: 65,
+        henchmen: 100_100_100,
+        info: "Born and raised in Dalarna",
+    },
 ];
 
+function printScientists() {
+    let cardContainer = document.querySelector(
+        "#scientist-cards"
+    ) as HTMLDivElement;
+
+    cardContainer.innerHTML = "";
+    for (let i = 0; i < Scientists.length; i++) {
+        const newScientistCard = document.createElement(
+            "div"
+        ) as HTMLDivElement;
+        newScientistCard.setAttribute("id", "column");
+        const cardlist = document.createElement("ul");
+        const nameList = document.createElement("div");
+        const cardButton = document.createElement("button");
+        cardButton.innerHTML = "More details";
+        nameList.innerHTML = Scientists[i].name;
+        cardContainer.appendChild(newScientistCard);
+        newScientistCard.appendChild(cardlist);
+        cardlist.appendChild(nameList);
+        nameList.appendChild(cardButton);
+
+        cardButton.addEventListener("click", () => {
+            printSelectedScientist(i);
+        });
+    }
+}
+
+printScientists();
+function printSelectedScientist(index: number) {
+    console.log("Clicked on index:" + index);
+    //Spara selected scientist i en variabel
+    const selectedScientist = Scientists[index];
+
+    let detailedInfoContainer = document.querySelector(
+        "#detailed-info"
+    ) as HTMLDivElement;
+    detailedInfoContainer.innerHTML = "";
+
+    const selectedScientistCard = document.createElement("div") as HTMLElement;
+    selectedScientistCard.setAttribute("id", "selected-scientist-card");
+    const selectedScientistName = document.createElement("h4");
+    selectedScientistName.innerHTML = selectedScientist.name;
+    const selectedScientistCardList = document.createElement(
+        "ul"
+    ) as HTMLUListElement;
+
+    const selectedScientistAge = document.createElement(
+        "div"
+    ) as HTMLDivElement;
+    selectedScientistAge.innerHTML = selectedScientist.age.toString();
+    const selectedScientistHenchmen = document.createElement(
+        "li"
+    ) as HTMLLIElement;
+    selectedScientistHenchmen.innerHTML = selectedScientist.henchmen.toString();
+    const selectedScientistInfo = document.createElement("li") as HTMLLIElement;
+    selectedScientistInfo.innerHTML = selectedScientist.info;
+
+    detailedInfoContainer.appendChild(selectedScientistCard);
+    selectedScientistCard.appendChild(selectedScientistName);
+    selectedScientistName.appendChild(selectedScientistCardList);
+    selectedScientistCardList.appendChild(selectedScientistAge);
+    selectedScientistCardList.appendChild(selectedScientistHenchmen);
+    selectedScientistCardList.appendChild(selectedScientistInfo);
+
+    //hej
+
+    // let detailedCardContainer = document.querySelector(
+    //     "#detailed-info"
+    // ) as HTMLDivElement;
+    // detailedCardContainer.innerHTML = "";
+    // for (let i = 0; i < Scientists.length; i++)
+    //     const selectedScientistCard = document.createElement(
+    //         "div"
+    //     ) as HTMLDivElement;
+    // selectedScientistCard.setAttribute("id", "selected-card");
+    // const selectedScientistName = document.createElement("h3");
+    // selectedScientistName.innerHTML = Scientists[index].name;
+    // selectedCardList = document.createElement("ul");
+    // selectedScientistAgeList = document.createElement("li");
+    // selectedScientistHenchmenList = document.createElement("li");
+    // selectedScientistInfoList = document.createElement("li");
+    // detailedCardContainer.appendChild(selec);
+    //
+}
 console.log(Scientists);
 
-const form = document.querySelector("#form") as HTMLFormElement;
+const submitButton = document.querySelector("#addbutton") as HTMLButtonElement;
 const newScientistName = document.querySelector("#name") as HTMLInputElement;
 const newScientistAge = document.querySelector("#age") as HTMLInputElement;
 const newScientistHenchmen = document.querySelector(
@@ -22,8 +113,8 @@ const newScientistHenchmen = document.querySelector(
 ) as HTMLInputElement;
 const newScientistInfo = document.querySelector("#info") as HTMLInputElement;
 
-form.addEventListener("submit", (e: Event) => {
-    e.preventDefault();
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
     const newScientist: evilScientist = {
         name: newScientistName.value,
         age: parseInt(newScientistAge.value),
@@ -32,4 +123,5 @@ form.addEventListener("submit", (e: Event) => {
     };
     Scientists.push(newScientist);
     console.log(Scientists);
+    printScientists();
 });
